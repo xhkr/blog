564d5f94a4a4b9c28ff9f8025763cc019b8017f7
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2016-11-22 17:44:24 +0100

    Initial commit
    Created a not-found.php page

---
+++ not-found.php
@@ -0,0 +1,10 @@
+<?php require_once "./templates/header.php"; ?>
+    <main>
+        <h2>Sidan kunde inte hittas</h2>
+        <p>Tyvärr kan vi inte hitta den sida du letar efter. Det kan bero på att sidan inte längre finns eller att den har flyttats.</p>
+        <!-- TODO: Se över namngivningen av klasserna -->
+        <div class="flexboxWrapper">
+        <a href="./index.php" class="button">Gå till startsidan</a>
+        </div>
+    </main>
+<?php require_once "./templates/footer.php"; ?>
