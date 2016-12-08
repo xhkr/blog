bcf93a223bf2221ec7ffb374c15bf2ee9c55e00f
Flandy85 <Gustavsson09@hotmail.com>
2016-12-08 13:43:43 +0100

    Comment out form, added new option in the first form
    WIP: Alot of thing to do^^

--- archive.php
+++ archive.php
@@ -22,15 +22,6 @@
         $query = "SELECT posts.*, categories.name FROM posts LEFT JOIN categories ON posts.categoryid = categories.id WHERE published = 1 ORDER BY created DESC";
     }
 
-    //Determine if a variable is set and is not NULL
-     $month = "";
-    if(isset($_GET["month"]) ) { //Avoids error message
-        $sort = $_GET["month"];
-    }
-    if($month == "month" && $month = $months[0]) {
-        $query = "SELECT posts.*, categories.name FROM posts LEFT JOIN categories ON posts.categoryid = categories.id WHERE created = 1";
-    }
-
 
     if ($stmt->prepare($query)) {
         $stmt->execute();
@@ -39,19 +30,76 @@
 
     // TODO: Seperating in different months needs to be handled by PHP and SQL.
     // TODO: Get the styling right on buttons, select and svg.
-    $months = array("Januari",
-        "Februari",
-        "Mars",
-        "April",
-        "Maj",
-        "Juni",
-        "Juli",
-        "Augusti",
-        "September",
-        "Oktober",
-        "November",
-        "December"
+    $months = array(
+        array("Januari", 01),
+        array("Februari", 02),
+        array("Mars", 03),
+        array("April", 04),
+        array("Maj", 05),
+        array("Juni", 06),
+        array("Juli", 07),
+        array("Augusti", 08),
+        array("September", 09),
+        array("Oktober", 10),
+        array("November", 11),
+        array("December", 12),
         );
+
+        //Determine if a variable is set and is not NULL
+     $month = "";
+
+    if(isset($_GET["month"]) ) { //Avoids error message
+        $sort = $_GET["month"];
+        $query = "SELECT EXTRACT(MONTH FROM created) FROM posts
+        WHERE EXTRACT(MONTH FROM created) = 11";
+    }
+
+
+    if ($stmt->prepare($query)) {
+        $stmt->execute();
+        $stmt->bind_result($monthtest);
+        $stmt->fetch();
+        echo $monthtest;
+    }
+
+     // $shop = array(
+     //            array("monthTitle" => "Januari",
+     //                  "monthNumber" => 01
+     //                ),
+     //           array("monthTitle" => "Februari",
+     //                  "monthNumber" => 02
+     //                ),
+                // array("monthTitle "=> "Mars",
+                //       "monthNumber" => 03
+                //     ),
+                // array("monthTitle "=> "April",
+                //       "monthNumber" => 04
+                //     ),
+                // array("monthTitle" => "Maj",
+                //       "monthNumber" => 05
+                //     ),
+                // array("monthTitle" => "Juni",
+                //       "monthNumber" => 06
+                //     ),
+                // array("monthTitle" => "Juli",
+                //       "monthNumber" => 07
+                //     ),
+                // array("monthTitle" => "Augusti",
+                //       "monthNumber" => 08
+                //     ),
+                // array("monthTitle" => "September",
+                //       "monthNumber" => 09
+                //     ),
+                // array("monthTitle" => "Oktober",
+                //       "monthNumber" => 10
+                //     ),
+                // array("monthTitle" => "November",
+                //       "monthNumber" => 11
+                //     ),
+                // array("monthTitle" => "December",
+                //      "monthNumber" => 12
+                //     ),
+             // );
 ?>
 <main>
     <h1 class="margin-bottom-l">Arkiv</h1>
@@ -63,20 +111,12 @@
               <option value="asc">Tidigast publicerad</option>
               <option value="name">Sortera efter bokstavsordning (A-Z)</option>
           </select>
-          <button class="button button--small border-radius margin-bottom-l" type="submit">Sortera</button>
-          <!-- <svg class="icon select-arrows">
-            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#select-arrows"></use>
-          </svg> -->
-        </div>
-    </form>
-    <form method="GET" action="archive.php">
-        <label for="month">Filtrera arkivet månadsvis</label>
-        <div class="select-arrows">
-          <select class="form-field form-field__select" name="sort-month" id="sort-month">
+          <label for="sort">Filtrera månadsvis</label>
+          <select class="form-field form-field__select" name="sort" id="sort">
             <option>Alla</option>
-            <?php foreach ($months as $actualMonth): ?>
-              <option value="month"><?php echo $actualMonth ?></option>
-            <?php endforeach; ?>
+            <?php for($i = 0; $i < 12; $i++): ?>
+              <option value="month"><?php echo $months[$i][0]; ?></option>
+            <?php endfor; ?>
           </select>
           <button class="button button--small border-radius margin-bottom-l" type="submit">Sortera</button>
           <!-- <svg class="icon select-arrows">
@@ -84,12 +124,27 @@
           </svg> -->
         </div>
     </form>
+   <!--  <form method="GET" action="archive.php">
+        <label for="month">Filtrera arkivet månadsvis</label>
+        <div class="select-arrows">
+          <select class="form-field form-field__select" name="sort-month" id="sort-month">
+            <option>Alla</option>
+            <?php for($i = 0; $i < 12; $i++): ?>
+              <option value="month"><?php echo $months[$i][0]; ?></option>
+            <?php endfor; ?>
+          </select>
+          <button class="button button--small border-radius margin-bottom-l" type="submit">Sortera</button>
+          <!-- <svg class="icon select-arrows">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#select-arrows"></use>
+          </svg> -->
+       <!-- </div>
+    </form> -->
     <div class="list-wrapper">
         <!-- <h2>November 2016</h2> -->
         <ul class="no-padding">
-        <?php while (mysqli_stmt_fetch($stmt)): ?>
-            <li class="list-style-none"><span class="saffron-text primary-brand-font">[<?php echo formatDate($created); ?>]</span><a href="post.php?getpost=<?php echo $id ?>"><?php echo $title; ?></a></li>
-        <?php endwhile; ?>
+        <!-- <?php //while (mysqli_stmt_fetch($stmt)): ?>
+            <li class="list-style-none"><span class="saffron-text primary-brand-font">[<?php //echo formatDate($created); ?>]</span><a href="post.php?getpost=<?php //echo $id ?>"><?php //echo $title; ?></a></li>
+        <?php //endwhile; ?> -->
         </ul>
     </div>
 </main>
